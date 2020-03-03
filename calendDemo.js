83fc6879470c40fafeeb931b0afa19e7f289d31f
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-03 19:49:51 +0300

    Add raw table generation

--- calendDemo.js
+++ calendDemo.js
@@ -2,4 +2,8 @@ import {createCalendar} from "./createCalendar.js";
 
 const container = document.querySelector(".container");
 
-createCalendar(container);
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+
+createCalendar(container, year, month);
--- createCalendar.js
+++ createCalendar.js
@@ -1,3 +1,49 @@
 export function createCalendar(elem, year, month) {
-    elem.innerText = "Hello";
+    const firstDay = new Date(year, month, 1);
+
+    
+    let table = document.createElement("table");
+    elem.append(table);
+    let tr = addRow();
+    
+    let i = 1;
+    let firstWeekDay = getWeekDay(firstDay);
+    while(i < firstWeekDay) {
+        addCell(tr);
+        i++;
+    }
+
+    let currDay = firstDay;
+    
+    
+    while(currDay.getMonth() === month) {
+        addCell(tr, currDay.getDate());
+        if (currDay.getDay() === 0) {
+            tr = addRow();
+        }
+        currDay.setDate(currDay.getDate() + 1);
+    }
+
+    // let formatter = Intl.DateTimeFormat("ru", {weekday: "short"});
+
+
+
+    function addRow() {
+        let tr = document.createElement("tr");
+        table.append(tr);
+        return tr;
+    }
+
+    function addCell(tr, text = "") {
+        let td = document.createElement("td");
+        td.textContent = text;
+        tr.append(td);
+    }
+
+    function getWeekDay(day) {
+        let weekDay = day.getDay();
+        if (weekDay === 0) weekDay = 7;
+        return weekDay;
+    }
 }
+
