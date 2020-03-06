6ebeafaacc49fafb2b4661bf4fa1abebff8021fd
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-06 13:23:48 +0300

    Refactored adding styles

--- createCalendar.js
+++ createCalendar.js
@@ -6,24 +6,26 @@ export function createCalendar(elem, year, month) {
 
     const daysOfTheWeek = ["пн","вт","ср","чт","пт","сб","вс"];
     daysOfTheWeek.forEach(day => {
-        let cell = addCell(elem, day);
-        cell.classList.add("calend__week_day");
+        addCell(elem, day, ["calend__day", "calend__week_day"]);
     })
 
     const monthArr = genMonth(year, month - 1);
     
     monthArr.days.forEach(day => {
-        let cell = addCell(elem, day);
+        let styles = ["calend__day"];
 
         if (day === monthArr.currentDay) {
-            cell.classList.add("calend__day--current");
+            styles = [...styles, "calend__day--current"];
         }
+
+        addCell(elem, day, styles);
     })
     
-    function addCell(elem, text) {
+    
+    function addCell(elem, text, styles) {
         let div = document.createElement("div");
         div.textContent = text;
-        div.classList.add("calend__day");
+        div.classList.add(...styles);
         elem.append(div);
         return div;
     }
